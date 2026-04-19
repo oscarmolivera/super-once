e1fc893cf3cde1dcc15558e7e5bc67f32e2fbfed
Oscar M. Olivera <oscarmolivera@gmail.com>
2026-04-18 20:57:16 -0400

    [Akamai] fix: Puma 8 deprecation: on_worker_boot -> before_worker_boot

--- config/puma.rb
+++ config/puma.rb
@@ -25,6 +25,6 @@ state_path "#{shared_dir}/pids/puma.state"
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
-on_worker_boot do
+before_worker_boot do
   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 end
