6216cc8abd53ca181c02710d98c720364b83bea2
Oscar M. Olivera <oscarmolivera@gmail.com>
2026-04-20 08:14:31 -0400

    [P1-FPS] feat: Rails authentication generated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   root "pages#placeholder"
   get "up" => "rails/health#show", as: :rails_health_check
 end
